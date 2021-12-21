e63d5a3732fd931273f181320f90893cd48b3ce7
I'm Prayoga <72582850+P677hl@users.noreply.github.com>
2021-12-21 13:23:47 +0700

    Add files via upload

--- assets/js/myscript.sweetalert2.js
+++ assets/js/myscript.sweetalert2.js
@@ -1,1 +1,39 @@
+// Id elemen a
+var dwn1 = document.getElementById("pencet1");
+var dwn2 = document.getElementById("pencet2");
 
+// Sweetalert2
+/*
+Swal.fire({
+  title: '<strong>Support Me</strong>',
+  icon: 'info',
+  html:
+   '<p><a href="https://saweria.co/beerde" target="_blank"><b>Klik Disini</b></a>, untuk mendukung saya agar semakin semangat membuat config. Bisa lewat (Dana, Gopay, Ovo, Link Aja, dll)</p>',
+  showCloseButton: true,
+  showConfirmButton: false
+});
+*/
+dwn1.addEventListener("click", function() {
+  Swal.fire({
+    title: '<strong>Support Me</strong>',
+    icon: 'info',
+    html:
+      '<p><a href="https://saweria.co/beerde" target="_blank"><b>Klik Disini</b></a>, untuk mendukung saya agar semakin semangat membuat config. Bisa lewat (Dana, Gopay, Ovo, Link Aja, dll)</p>',
+    showCloseButton: true,
+    showConfirmButton: false
+  });
+  var nyawer1 = window.open('http://dukung.imprayoga.rf.gd/' ,'_blank');
+  nyawer1.focus();
+});
+dwn2.addEventListener("click", function() {
+  Swal.fire({
+    title: '<strong>Support Me</strong>',
+    icon: 'info',
+    html:
+      '<p><a href="https://saweria.co/beerde" target="_blank"><b>Klik Disini</b></a>, untuk mendukung saya agar semakin semangat membuat config. Bisa lewat (Dana, Gopay, Ovo, Link Aja, dll)</p>',
+    showCloseButton: true,
+    showConfirmButton: false
+  });
+  var nyawer2 = window.open('http://dukung.imprayoga.rf.gd/' ,'_blank');
+  nyawer2.focus();
+});
